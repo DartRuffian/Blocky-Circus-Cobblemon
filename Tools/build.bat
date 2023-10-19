ae7b5062b86262002cc17711ca27df8b8e0e964a
DartRuffian <skulldude327@gmail.com>
2023-10-19 00:44:21 -0500

    Working build script

---
+++ Tools/build.bat
@@ -0,0 +1,6 @@
+set source="C:\Coding\GitHub Repos\Blocky-Circus-Cobblemon\datapacks"
+set target="%appdata%\com.modrinth.theseus\profiles\BC-Cobblemon Test\saves\Origin Testing\datapacks"
+
+rmDir /s /q %target%
+
+ROBOCOPY %source% %target% /MIR
